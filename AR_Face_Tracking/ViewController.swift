4d6dfdc876d0918a6d393f12d4427bfae06ed335
Chris Paine <cpaine@taxslayer.com>
2019-01-15 17:23:10 -0500

    Initial Commit

---
+++ AR_Face_Tracking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR_Face_Tracking
+//
+//  Created by Chris Paine on 1/15/19.
+//  Copyright © 2019 Chris Paine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
